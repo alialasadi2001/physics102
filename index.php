ca3d4b79234c75dc6ee8a850a73cee286282f7bd
alialasadi2001 <alasadi2001@hotmail.com>
2022-01-24 12:26:30 +0300

    UI is done

--- index.php
+++ index.php
@@ -15,10 +15,17 @@
 
 
 
-      <form class="" action="index.html" method="post">
+      <form action="index.html" method="post">
         <div class="mb-3">
         <label for="formGroupExampleInput" class="form-label">Search the question</label>
         <textarea type="text" class="form-control" id="formGroupExampleInput" placeholder="Type your question"></textarea>
+        <button style="  cursor: pointer;
+          background-color: rgb(13, 122, 0) ;
+          border: 2px solid rgb(13, 122, 0) ;
+          border-radius: 4px;
+          color: #fff;
+          padding: 10px;
+          margin-top: 20px;"type="submit" name="button">Submit</button>
       </div>
       </form>
       </div>
