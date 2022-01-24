705ef022d2e30b91f84cf1d1bc4678d842464ad9
alialasadi2001 <alasadi2001@hotmail.com>
2022-01-24 12:48:11 +0300

    Fixed UI with unified layout

---
+++ footer.php
@@ -0,0 +1,7 @@
+</div>
+</div>
+<div class="bottom">
+<h3>Physics102 website is designed for educational purposes.</h3>
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,19 +1,7 @@
 <?php
-
- ?><!DOCTYPE html>
- <html lang="en" dir="ltr">
-   <head>
-     <meta charset="utf-8">
-     <title>Physics102</title>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-     <link rel="stylesheet" href="styles.css">
-   </head>
-   <body>
-
-     <div class="centerBlock">
-       <div class="container-fluid">
-
-
+require('header.php');
+ ?>
+--------
 
       <form method="post">
         <div class="mb-3">
@@ -28,10 +16,5 @@
           margin-top: 20px;"type="submit" name="button">Submit</button>
       </div>
       </form>
-      </div>
-    </div>
-    <div class="bottom">
-      <h3>Physics102 website is designed for educational purposes.</h3>
-    </div>
-   </body>
- </html>
+
+<?php require('footer.php'); ?>
