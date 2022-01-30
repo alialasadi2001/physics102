1c458bc881fe34ab7a4c3cc4227d684835210c48
zanntalaq <zainabtalaq7@gmail.com>
2022-01-30 18:03:47 +0300

    //

---
+++ six.php
@@ -0,0 +1,24 @@
+<?php
+    require('header.php');
+?>
+<form method="post">
+    <p>The electric potential in a certain region is given by
+        v
+    </p>
+    <img src="six.png" style=" width:100px; height:100px; ">
+    <br>
+    <table>
+        <p> input the missing value ()</P>
+        
+
+        <br>
+        <br>
+        <input type="Submit" value="Submit">
+            
+    </table>
+</form>
+
+
+<?php
+ require('footer.php'); 
+?>
